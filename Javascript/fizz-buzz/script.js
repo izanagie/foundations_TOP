8cefc121f5f6a85ff1c5e0fce754c38a18a44b7b
izanagie <bvishnu181298@gmail.com>
2023-09-18 19:12:11 +0530

    added fizz

---
+++ Javascript/fizz-buzz/script.js
@@ -0,0 +1,14 @@
+let number = prompt("ente a number")
+
+function fizzBuzz(){
+    for(let i=0; i<=number;i++){
+        if(i%3 == 0){
+            console.log("fizz")
+        }else if( i%5 == 0){
+            console.log("buzz")
+        }
+        console.log(i)
+    }    
+}
+
+fizzBuzz()
